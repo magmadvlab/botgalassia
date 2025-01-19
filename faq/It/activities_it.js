1ff64094e872b4f2bf83a07db0c240bdd3361f03
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-19 09:20:21 +0100

    Create activities_it.js

---
+++ faq/It/activities_it.js
@@ -0,0 +1,22 @@
+export const activitiesFAQ_IT = {
+  title: 'Attività e Servizi Extra',
+  keywords: ['motoslitta', 'ciaspole', 'village', 'snowtubing', 'attività'],
+  questions: {
+    'Come posso prenotare un giro in motoslitta?': {
+      answer: 'Puoi chiamare il numero +39 349 144 4433.',
+      tags: ['motoslitta', 'prenotazioni']
+    },
+    'Dove si organizzano le ciaspolate?': {
+      answer: 'Puoi chiamare il numero +39 0174 334 151.',
+      tags: ['ciaspole', 'escursioni']
+    },
+    'Come si raggiunge il Prato Nevoso Village?': {
+      answer: 'Dall\'hotel vi accompagnamo in centro con la nostra navetta, lì dove ci sono tutte le attività troverete Prato Nevoso Village.',
+      tags: ['village', 'attività']
+    },
+    'Come posso prenotare attività extra come snowtubing?': {
+      answer: 'Puoi chiamare il numero +39 0174 334 151.',
+      tags: ['snowtubing', 'prenotazioni']
+    }
+  }
+};

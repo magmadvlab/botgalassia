f4b15e38dada657619db7cc54e6b81c76ae09d38
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-19 09:21:30 +0100

    Create emergency_it.js

---
+++ faq/It/emergency_it.js
@@ -0,0 +1,26 @@
+export const emergencyFAQ_IT = {
+  title: 'Emergenze e Assistenza',
+  keywords: ['emergenza', 'medico', 'farmacia', 'aiuto', 'assistenza', 'reception'],
+  questions: {
+    'Qual è il numero della reception?': {
+      answer: '+39 0174 334183',
+      tags: ['contatti', 'reception']
+    },
+    'C\'è un medico disponibile in hotel?': {
+      answer: 'No, ma è presente un servizio di guardia medica a Prato Nevoso.',
+      tags: ['medico', 'assistenza sanitaria']
+    },
+    'Dove si trova la farmacia più vicina?': {
+      answer: 'La farmacia più vicina si trova in Conca a Pratonevoso.',
+      tags: ['farmacia', 'medicinali']
+    },
+    'Come contatto il personale in caso di emergenza notturna?': {
+      answer: 'Chiamare sempre il numero della reception: +39 0174 334183. Il numero della reception ha il trasferimento di chiamata su un numero di cellulare.',
+      tags: ['emergenza', 'notte', 'contatti']
+    },
+    'È disponibile un numero di emergenza alternativo?': {
+      answer: 'Il numero della reception ha il trasferimento di chiamata su un numero di cellulare per le emergenze.',
+      tags: ['emergenza', 'contatti']
+    }
+  }
+};

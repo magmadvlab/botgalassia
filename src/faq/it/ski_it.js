b8433117bff3dbb4068ce37ea35c9a2170eba9e5
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-20 13:58:28 +0100

    Update ski_it.js

--- src/faq/it/ski_it.js
+++ src/faq/it/ski_it.js
@@ -1,10 +1,10 @@
 export const skiFAQ_IT = {
   title: 'Servizi Sci e Sport',
-  keywords: ['sci', 'pista', 'skibox', 'ski box', 'neve', 'skipass', 'noleggio', 'scuola sci'],
+  keywords: ['sci', 'pista', 'skibox', 'ski box', 'neve', 'skipass', 'noleggio', 'scuola sci', 'principiante', 'imparare'],
   questions: {
-    'Come si raggiungono le piste da sci?': {
-      answer: 'Per raggiungere il tunnel bisogna prendere l\'ascensore e andare al piano -1, andare a destra e attraverso un tunnel che porta direttamente alla pista blu nr. 1 "pista del Prel".',
-      tags: ['piste', 'accesso', 'tunnel']
+    'Come faccio per andare a sciare?': {
+      answer: 'Se sei già capace di sciare: puoi accedere direttamente alle piste attraverso il tunnel dell\'hotel. Prendi l\'ascensore fino al piano -1, vai a destra e attraversa il tunnel che ti porterà direttamente sulla pista blu nr. 1 "pista del Prel". Se hai bisogno di attrezzatura, il nostro partner Beppe Ski si trova proprio di fronte all\'uscita del tunnel e offre uno sconto del 10% agli ospiti dell\'hotel. Se invece non hai mai sciato, puoi utilizzare il nostro servizio navetta gratuito (attivo dalle 08:30 alle 12:30 e dalle 14:30 alle 17:30, da prenotare in reception) che ti porterà in conca, dove troverai diverse scuole di sci con maestri qualificati per imparare. Anche in questo caso, per il noleggio dell\'attrezzatura, ti consigliamo Beppe Ski con il 10% di sconto riservato ai nostri ospiti.',
+      tags: ['piste', 'accesso', 'tunnel', 'noleggio', 'principiante', 'scuola sci', 'navetta']
     },
     'Dove si trovano gli ski box?': {
       answer: 'Al piano -1. Bisogna percorrere tutto il tunnel, ogni ski box è contrassegnato da un numero.',
@@ -19,8 +19,8 @@ export const skiFAQ_IT = {
       tags: ['navetta', 'trasporto']
     },
     'C\'è un noleggio sci convenzionato?': {
-      answer: 'Sì, Bepe Ski con tariffe speciali per gli ospiti.',
-      tags: ['noleggio', 'attrezzatura']
+      answer: 'Sì, Beppe Ski, situato proprio di fronte all\'uscita del tunnel dell\'hotel, offre uno sconto del 10% agli ospiti dell\'hotel sul noleggio dell\'attrezzatura e assistenza.',
+      tags: ['noleggio', 'attrezzatura', 'sconto']
     },
     'Come posso prenotare le lezioni di sci?': {
       answer: 'L\'Hotel non ha un servizio convenzionato con le scuole, ma se prenoti da Beppe Ski saprà fornirti tutte le indicazioni del caso.',

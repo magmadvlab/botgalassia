9795ac428186558cec7399e2951e2ed99fb170bb
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-20 11:36:25 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
 }

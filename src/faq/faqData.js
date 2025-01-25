590eb696d869c9f890b413972ae967940c608436
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-25 16:43:48 +0100

    Update faqData.js
    Signed-off-by: magmadvlab <35226689+magmadvlab@users.noreply.github.com>

--- src/faq/faqData.js
+++ src/faq/faqData.js
@@ -1,14 +1,14 @@
 // Import delle FAQ individuali
-import activitiesAndAttractions_IT from './it/activities_and_attractions_faq_it.js';
-import checkin_IT from './it/checkin_it';
-import checkout_IT from './it/checkout_it';
-import dining_IT from './it/dining_it';
-import emergency_IT from './it/emergency_it';
-import pets_IT from './it/pets_it';
-import ski_IT from './it/ski_it';
-import techServices_IT from './it/tech_services_it';
-import transport_IT from './it/transport_it';
-import wellness_IT from './it/wellness_it';
+Copyimport activitiesAndAttractions_IT from './it/activities_and_attractions_faq_it.js';
+import checkin_IT from './it/checkin_it.js';
+import checkout_IT from './it/checkout_it.js';
+import dining_IT from './it/dining_it.js';
+import emergency_IT from './it/emergency_it.js';
+import pets_IT from './it/pets_it.js';
+import ski_IT from './it/ski_it.js';
+import techServices_IT from './it/tech_services_it.js';
+import transport_IT from './it/transport_it.js';
+import wellness_IT from './it/wellness_it.js';
 
 const faqData = {
   activitiesAndAttractions_IT,

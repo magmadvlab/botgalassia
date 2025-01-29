ed5a406146dbeef8e41ec7ea0009ec2201c97611
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-29 20:35:57 +0100

    Update faqData.js
    Signed-off-by: magmadvlab <35226689+magmadvlab@users.noreply.github.com>

--- src/faq/it/faqData.js
+++ src/faq/it/faqData.js
@@ -1,25 +1,27 @@
-import { activitiesFAQ_IT } from './activities_it';
-import { checkinFAQ_IT } from './checkin_it';
-import { checkoutFAQ_IT } from './checkout_it';
-import { diningFAQ_IT } from './dining_it';
-import { emergencyFAQ_IT } from './emergency_it';
-import { petsFAQ_IT } from './pets_it';
-import { skiFAQ_IT } from './ski_it';
-import { techServicesFAQ_IT } from './tech_services_it';
-import { transportFAQ_IT } from './transport_it';
-import { wellnessFAQ_IT } from './wellness_it';
+// Importazioni dei dati FAQ dall'italiano
+import { activities_it } from './activities_it';
+import { checkin_it } from './checkin_it';
+import { checkout_it } from './checkout_it';
+import { dining_it } from './dining_it';
+import { emergency_it } from './emergency_it';
+import { pets_it } from './pets_it';
+import { ski_it } from './ski_it';
+import { tech_services_it } from './tech_services_it';
+import { transport_it } from './transport_it';
+import { wellness_it } from './wellness_it';
 
+// Configurazione dei dati FAQ
 const faqData = {
-    activities: activitiesFAQ_IT,
-    checkin: checkinFAQ_IT,
-    checkout: checkoutFAQ_IT,
-    dining: diningFAQ_IT,
-    emergency: emergencyFAQ_IT,
-    pets: petsFAQ_IT,
-    ski: skiFAQ_IT,
-    techServices: techServicesFAQ_IT,
-    transport: transportFAQ_IT,
-    wellness: wellnessFAQ_IT
+    activities: activities_it,
+    checkin: checkin_it,
+    checkout: checkout_it,
+    dining: dining_it,
+    emergency: emergency_it,
+    pets: pets_it,
+    ski: ski_it,
+    techServices: tech_services_it,
+    transport: transport_it,
+    wellness: wellness_it
 };
 
 export default faqData;

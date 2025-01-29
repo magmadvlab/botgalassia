e8e2b4be114503aadea800718ee370c236808005
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-29 19:51:52 +0100

    Update faqData.js
    Signed-off-by: magmadvlab <35226689+magmadvlab@users.noreply.github.com>

--- src/faq/it/faqData.js
+++ src/faq/it/faqData.js
@@ -1,4 +1,3 @@
-// Importa i dati FAQ in italiano
 import { activitiesFAQ_IT } from './activities_it';
 import { checkinFAQ_IT } from './checkin_it';
 import { checkoutFAQ_IT } from './checkout_it';
@@ -10,24 +9,17 @@ import { techServicesFAQ_IT } from './tech_services_it';
 import { transportFAQ_IT } from './transport_it';
 import { wellnessFAQ_IT } from './wellness_it';
 
-// Oggetto che contiene tutti i dati FAQ
 const faqData = {
-  activities: activitiesFAQ_IT,
-  checkin: checkinFAQ_IT,
-  checkout: checkoutFAQ_IT,
-  dining: diningFAQ_IT,
-  emergency: emergencyFAQ_IT,
-  pets: petsFAQ_IT,
-  ski: skiFAQ_IT,
-  techServices: techServicesFAQ_IT,
-  transport: transportFAQ_IT,
-  wellness: wellnessFAQ_IT,
+    activities: activitiesFAQ_IT,
+    checkin: checkinFAQ_IT,
+    checkout: checkoutFAQ_IT,
+    dining: diningFAQ_IT,
+    emergency: emergencyFAQ_IT,
+    pets: petsFAQ_IT,
+    ski: skiFAQ_IT,
+    techServices: techServicesFAQ_IT,
+    transport: transportFAQ_IT,
+    wellness: wellnessFAQ_IT
 };
 
-// Debug log per verificare il contenuto
-if (process.env.NODE_ENV === 'development') {
-  console.log('FAQ Data:', faqData);
-}
-
-// Esporta l'oggetto faqData come default
 export default faqData;

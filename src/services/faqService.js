e115ad0d8121e3dd423e853c7f14c546260b2c50
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-02-02 17:18:54 +0100

    Update faqService.js

--- src/services/faqService.js
+++ src/services/faqService.js
@@ -47,7 +47,7 @@ const expandInput = (userInput) => {
 // Configurazione della ricerca fuzzy
 const fuseOptions = {
   keys: [
-    { name: 'tags', weight: 6 },
+    { name: 'tags', weight: 7 },
     { name: 'category', weight: 2 },
     { name: 'question', weight: 1 }
   ],
